e357c4d665e5c0c0b170453fe6502d928afe2680
wancat <wancat@pm.me>
2022-02-26 12:38:00 +0800

    Change indent

--- query.ts
+++ query.ts
@@ -1,11 +1,13 @@
-import { ISCNQueryClient } from '@likecoin/iscn-js';
+import { ISCNQueryClient } from "@likecoin/iscn-js";
 
 const client = new ISCNQueryClient();
 
 async function main() {
-	await client.connect('http://node.testnet.like.co/rpc/');
-	const res = await client.queryRecordsByOwner('cosmos170d84tujgds00qcckxmd3jv0p222c4909rn7yg');
-	console.dir(res, { depth: null});
+  await client.connect("http://node.testnet.like.co/rpc/");
+  const res = await client.queryRecordsByOwner(
+    "cosmos170d84tujgds00qcckxmd3jv0p222c4909rn7yg"
+  );
+  console.dir(res, { depth: null });
 }
 
 main();
--- sign.js
+++ sign.js
@@ -1,43 +1,46 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { ISCNQueryClient, ISCNSigningClient } from '@likecoin/iscn-js';
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
-import { SigningStargateClient, StargateClient } from '@cosmjs/stargate';
+import { ISCNQueryClient, ISCNSigningClient } from "@likecoin/iscn-js";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { SigningStargateClient, StargateClient } from "@cosmjs/stargate";
 
-const MNEMONIC = process.env['MNEMONIC'];
+const MNEMONIC = process.env["MNEMONIC"];
 console.log(MNEMONIC);
 
 async function sign() {
-	const signer = await DirectSecp256k1HdWallet.fromMnemonic(MNEMONIC);
-	const [wallet] = await signer.getAccounts();
-	console.log(wallet);
+  const signer = await DirectSecp256k1HdWallet.fromMnemonic(MNEMONIC);
+  const [wallet] = await signer.getAccounts();
+  console.log(wallet);
 
-	const signingClient = new ISCNSigningClient();
-	await signingClient.connectWithSigner('http://node.testnet.like.co/rpc/', wallet);
-	const client = new ISCNQueryClient();
-	await client.connect('http://node.testnet.like.co/rpc/');
+  const signingClient = new ISCNSigningClient();
+  await signingClient.connectWithSigner(
+    "http://node.testnet.like.co/rpc/",
+    wallet
+  );
+  const client = new ISCNQueryClient();
+  await client.connect("http://node.testnet.like.co/rpc/");
 
-	const res = await signingClient.createISCNRecord(wallet.address, {
-		'contentFingerPrints': [
-			"hash://sha256/9564b85669d5e96ac969dd0161b8475bbced9e5999c6ec598da718a3045d6f2e",
-		],
-		'stakeholders': [
-			{
-				'entity': {
-					'@id': wallet.address,
-					'name': 'Justin Lin',
-				},
-				'rewardProportion': 100,
-				'contributionType': "http://schema.org/author",
-			}
-		],
-		'type': 'Message',
-		'name': 'Test ISCN',
-	})
-	console.log(res);
-	const iscnID = await client.queryISCNIdsByTx(res.transactionHash);
-	console.log(iscnID)
+  const res = await signingClient.createISCNRecord(wallet.address, {
+    contentFingerPrints: [
+      "hash://sha256/9564b85669d5e96ac969dd0161b8475bbced9e5999c6ec598da718a3045d6f2e",
+    ],
+    stakeholders: [
+      {
+        entity: {
+          "@id": wallet.address,
+          name: "Justin Lin",
+        },
+        rewardProportion: 100,
+        contributionType: "http://schema.org/author",
+      },
+    ],
+    type: "Message",
+    name: "Test ISCN",
+  });
+  console.log(res);
+  const iscnID = await client.queryISCNIdsByTx(res.transactionHash);
+  console.log(iscnID);
 }
 
 sign();
