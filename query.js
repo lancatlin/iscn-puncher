e84acc911afd3af35425bde1b049cba583e07951
wancat <wancat@wancat.cc>
2022-03-05 17:51:25 +0800

    Organize query records

--- query.js
+++ query.js
@@ -1,24 +1,49 @@
 const { ISCNQueryClient } = require("@likecoin/iscn-js");
 const { loadWallet, ENDPOINT } = require("./wallet");
+const { PUNCH_IN, PUNCH_OUT } = require("./sign");
 
 const client = new ISCNQueryClient();
 
-async function query(
-  startFrom = new Date("2022-02-27T12:40:00+08:00"),
-  until = new Date(),
-) {
+async function query() {
   const { wallet } = await loadWallet();
   await client.connect(ENDPOINT);
   const { records } = await client.queryRecordsByOwner(wallet.address);
+  console.log(organize(records));
+}
+
+function organize(records) {
+  const result = [];
+  let last = null;
   for (const record of records) {
     const timestamp = new Date(record.data.recordTimestamp);
+    const { keywords } = record.data.contentMetadata;
+    if (keywords === PUNCH_IN) {
+      last = record;
+    }
+    if (keywords === PUNCH_OUT && last) {
+      result.push(commit(last, record));
+      last = null;
+    }
+
     console.log(
-      timestamp,
-      startFrom.valueOf() < timestamp.valueOf() &&
-        timestamp.valueOf() < until.valueOf()
+      keywords, timestamp,
     );
   }
-  console.log(startFrom, until);
+  return result;
+}
+
+function commit(recordIn, recordOut) {
+  const fromTime = new Date(recordIn.data.recordTimestamp);
+  const toTime = new Date(recordOut.data.recordTimestamp);
+  const duration = toTime.getTime() - fromTime.getTime();
+  const minutes = Math.ceil(duration / (1000 * 60));
+  return {
+    fromTime,
+    fromId: recordIn.data['@id'],
+    toTime,
+    toId: recordOut.data['@id'],
+    duration: minutes,
+  };
 }
 
 module.exports = {
